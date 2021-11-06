ed93bce18d14329a92c87d76518692b06b1d1fdc
Connor Slade <connor@connorcode.com>
2021-11-05 22:19:28 -0400

    💪 Coments

--- src/routes/randomimg/get_image.rs
+++ src/routes/randomimg/get_image.rs
@@ -1,4 +1,5 @@
 use std::io::Read;
+use std::time::Duration;
 
 use afire::Header;
 use afire::Method;
@@ -11,13 +12,17 @@ use ureq;
 
 pub fn attach(server: &mut Server) {
     server.route(Method::GET, "/randomimg/image.png", |_req| {
+        // Try to find a ramdom image 5 times
         for _ in 1..5 {
             match get_random_image() {
+                // If image found, send it to client
                 Some(i) => {
                     return Response::new()
                         .bytes(i)
                         .header(Header::new("Content-Type", "image/png"))
                 }
+
+                // If not, try again
                 None => {}
             }
         }
@@ -26,12 +31,18 @@ pub fn attach(server: &mut Server) {
     });
 }
 
+/// Try to get a random Lightshot Image
 fn get_random_image() -> Option<Vec<u8>> {
+    // Gen Posable Lightshot Image ID
     let chars: String = (1..6)
         .map(|_| thread_rng().sample(Alphanumeric) as char)
         .collect();
 
-    let agent = ureq::Agent::new();
+    let agent = ureq::AgentBuilder::new()
+        .timeout(Duration::from_secs(1))
+        .build();
+
+    // Get Image page
     let i = agent
         .get(&format!("https://prnt.sc/{}", chars.to_lowercase()))
         .call()
@@ -39,13 +50,15 @@ fn get_random_image() -> Option<Vec<u8>> {
         .into_string()
         .ok()?;
 
+    // Try to extract image url
     let j = extract_image_url(i)?;
 
-    // Check if is a screenshot was removed message
+    // Check if is a screenshot that was removed
     if j == "//st.prntscr.com/2021/10/22/2139/img/0_173a7b_211be8ff.png" {
         return None;
     }
 
+    // Load Screenshot
     let mut buf = Vec::new();
     agent
         .get(&j)
@@ -55,9 +68,11 @@ fn get_random_image() -> Option<Vec<u8>> {
         .read_to_end(&mut buf)
         .unwrap();
 
+    // Respond with Screenshot Bytes
     Some(buf)
 }
 
+/// Extract Image Url from Lightshot Page
 fn extract_image_url(body: String) -> Option<String> {
     Some(
         body.split(r#"<img class="no-click screenshot-image" src=""#)
