16caae532b6a409303dc7eb1bc42da608bcb518b
Connor Slade <connor@connorcode.com>
2021-11-07 20:42:29 -0500

    🎹 Pi in its own file

--- data/static/Pi/index.js
+++ data/static/Pi/index.js
@@ -5,3 +5,7 @@ document.querySelector('#gist112881990').style.display = 'none';
 code.addEventListener('click', () => {
   document.querySelector('#gist112881990').style.display = 'block';
 });
+
+fetch('/pi/pi.txt').then(r => r.text()).then(r => {
+  document.querySelector('#pi').innerHTML = r;
+})
