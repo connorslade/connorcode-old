398961d0b9b4101915f234ed6772ee8d7106a2c4
Connor Slade <connor@connorcode.com>
2021-11-29 21:00:18 -0500

    💅 Rename Password

--- src/config.rs
+++ src/config.rs
@@ -18,9 +18,10 @@ lazy_static! {
     pub static ref ANALYTICS_ENABLED: bool = config!().get::<bool>("analytics_enabled").unwrap();
     pub static ref ANALYTICS_SERVE: bool = config!().get::<bool>("analytics_serve").unwrap();
     pub static ref ANALYTICS_PATH: String = config!().get_str("analytics_path").unwrap();
-    pub static ref ANALYTICS_PASS: String = config!().get_str("analytics_pass").unwrap();
     pub static ref DUMP_PEROID: u64 = config!().get::<u64>("dump_peroid").unwrap();
 
+    // Other
+    pub static ref PASS: String = config!().get_str("pass").unwrap();
 }
 
 pub fn load(path: &str) -> Option<()> {
