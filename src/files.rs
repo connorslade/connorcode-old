21ab31c3f2633ff24585a025210ad973c8f8ed9b
Connor Slade <connor@connorcode.com>
2021-12-09 21:11:04 -0500

    🎉 Add MIME types to file server

--- src/files.rs
+++ src/files.rs
@@ -18,7 +18,7 @@ pub fn attach(server: &mut Server) {
         }
 
         let file_path = req.path.replace("/..", "");
-        let mut file_path = file_path.strip_prefix("/files").unwrap().to_owned();
+        let mut file_path = file_path.strip_prefix("/files")?.to_owned();
 
         while file_path.starts_with('/') {
             file_path = file_path.replacen("/", "", 1);
@@ -43,15 +43,18 @@ pub fn attach(server: &mut Server) {
             let mut out = String::new();
 
             for i in dir {
-                let j = i.to_str().unwrap();
-                let url = j.split(&*FILE_SERVE_PATH).nth(1).unwrap().to_owned();
-                let name = j.split(path.to_str().unwrap()).nth(1).unwrap().to_owned();
+                let j = i.to_str()?;
+                let url = j.split(&*FILE_SERVE_PATH).nth(1)?.to_owned();
+                let name = j.split(path.to_str()?).nth(1)?.to_owned();
 
                 out.push_str(&format!(
                     r#"<div class="file"><a href="/files{}"><i class="fa fa-{}"></i> {}</a></div>"#,
                     url,
                     if i.is_file() { "file" } else { "folder" },
-                    name.strip_prefix('/').unwrap_or(&name)
+                    name.strip_prefix('/')
+                        .unwrap_or(&name)
+                        .strip_prefix('\\')
+                        .unwrap_or(&name)
                 ));
             }
 
@@ -62,6 +65,7 @@ pub fn attach(server: &mut Server) {
                             fs::read_to_string("./data/template/files.html")
                                 .unwrap_or_else(|_| "{{FILES}}".to_owned()),
                         )
+                        .template("PATH", path.file_name()?.to_str()?)
                         .template("FILES", out)
                         .template("VERSION", VERSION)
                         .build(),
@@ -70,7 +74,7 @@ pub fn attach(server: &mut Server) {
             );
         }
 
-        let file = match fs::read(path) {
+        let file = match fs::read(&path) {
             Ok(i) => i,
             Err(_) => {
                 return Some(
@@ -80,10 +84,46 @@ pub fn attach(server: &mut Server) {
                 )
             }
         };
-        return Some(
-            Response::new()
-                .bytes(file)
-                .header(Header::new("Content-Type", "text/plain; charset=utf-8")),
-        );
+
+        if req.query.get("download").is_some() {
+            return Some(
+                Response::new()
+                    .bytes(file)
+                    .header(Header::new("Content-Type", "application/octet-stream")),
+            );
+        }
+
+        Some(
+            show_response(path)
+                .unwrap_or_else(|| {
+                    Response::new().header(Header::new("Content-Type", "application/octet-stream"))
+                })
+                .bytes(file),
+        )
     }));
+}
+
+fn show_response(file: PathBuf) -> Option<Response> {
+    let content_type = match file.extension()?.to_str()?.to_lowercase().as_str() {
+        "txt" => "text/plain; charset=utf-8",
+        "html" => "text/html; charset=utf-8",
+        "css" => "text/css; charset=utf-8",
+        "js" => "text/javascript; charset=utf-8",
+        "json" => "application/json; charset=utf-8",
+        "png" => "image/png",
+        "jpg" => "image/jpeg",
+        "jpeg" => "image/jpeg",
+        "gif" => "image/gif",
+        "svg" => "image/svg+xml",
+        "ico" => "image/x-icon",
+        "woff" => "application/font-woff",
+        "woff2" => "application/font-woff2",
+        "ttf" => "application/font-ttf",
+        "otf" => "application/font-otf",
+        "mp3" => "audio/mpeg",
+        "mp4" => "video/mp4",
+        _ => "application/octet-stream",
+    };
+
+    Some(Response::new().header(Header::new("Content-Type", content_type)))
 }
