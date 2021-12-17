daecc03bb4b0cd13244bc9e6516abbc22e1cd947
Connor Slade <connor@connorcode.com>
2021-12-16 21:58:18 -0500

    🌋 Basic File Server Error Handling

--- src/files.rs
+++ src/files.rs
@@ -1,7 +1,7 @@
 use std::fs;
 use std::path::PathBuf;
 
-use afire::{Header, Method, Response, Server};
+use afire::{Header, Method, Request, Response, Server};
 
 use crate::config::{FILE_SERVE, FILE_SERVE_PATH};
 use crate::Template;
@@ -13,106 +13,124 @@ pub fn attach(server: &mut Server) {
     }
 
     server.middleware(Box::new(|req| {
-        if !req.path.starts_with("/files") || req.method != Method::GET {
-            return None;
-        }
-
-        let file_path = req.path.replace("/..", "");
-        let mut file_path = file_path.strip_prefix("/files")?.to_owned();
-
-        while file_path.starts_with('/') {
-            file_path = file_path.replacen("/", "", 1);
-        }
-
-        let path = PathBuf::from(FILE_SERVE_PATH.clone()).join(&file_path);
-
-        if path.is_dir() {
-            let mut dir = match fs::read_dir(&path) {
-                Ok(i) => i,
-                Err(_) => {
-                    return Some(
-                        Response::new()
-                            .status(404)
-                            .text(format!("Folder `{}` not found", file_path)),
-                    )
-                }
-            }
-            .map(|x| x.unwrap().path())
-            .collect::<Vec<PathBuf>>();
-
-            dir.sort();
-
-            let mut out = String::new();
-
-            if path != PathBuf::from(FILE_SERVE_PATH.clone()) {
-                out.push_str(
-                    &format!(r#"<div class="file"><a href="/files{}"><i class="fa fa-folder"></i> ..</a></div>"#,
-                    path.parent().unwrap().as_os_str().to_string_lossy().replacen(FILE_SERVE_PATH.as_str(), "", 1))
-                );
-            }
-
-            for i in dir {
-                let j = i.to_str()?;
-                let url = j.split(&*FILE_SERVE_PATH).nth(1)?.to_owned();
-                let name = j.split(path.to_str()?).nth(1)?.to_owned();
-
-                out.push_str(&format!(
-                    r#"<div class="file"><a href="/files{}"><i class="fa fa-{}"></i> {}</a></div>"#,
-                    url,
-                    match i.is_file() {
-                        true => "file",
-                        _ => "folder",
-                    },
-                    name.strip_prefix('/')
-                        .unwrap_or(&name)
-                        .strip_prefix('\\')
-                        .unwrap_or(&name)
-                ));
-            }
-
-            return Some(
-                Response::new()
-                    .text(
-                        Template::new(
-                            fs::read_to_string("./data/template/files.html")
-                                .unwrap_or_else(|_| "{{FILES}}".to_owned()),
-                        )
-                        .template("PATH", path.file_name()?.to_str()?)
-                        .template("FILES", out)
+        run(req).unwrap_or(Some(
+            Response::new()
+                .status(500)
+                .text(
+                    Template::new(include_str!("../data/template/error.html"))
                         .template("VERSION", VERSION)
+                        .template("ERROR", "smthng")
                         .build(),
-                    )
-                    .header(Header::new("Content-Type", "text/html; charset=utf-8")),
-            );
-        }
+                )
+                .header(Header::new("Content-Type", "text/html")),
+        ))
+    }));
+}
 
-        let file = match fs::read(&path) {
+fn run(req: &Request) -> Option<Option<Response>> {
+    if !req.path.starts_with("/files") || req.method != Method::GET {
+        return Some(None);
+    }
+
+    let file_path = req.path.replace("/..", "");
+    let mut file_path = file_path.strip_prefix("/files")?.to_owned();
+
+    while file_path.starts_with('/') {
+        file_path = file_path.replacen("/", "", 1);
+    }
+
+    let path = PathBuf::from(FILE_SERVE_PATH.clone()).join(&file_path);
+
+    if path.is_dir() {
+        let mut dir = match fs::read_dir(&path) {
             Ok(i) => i,
             Err(_) => {
-                return Some(
+                return Some(Some(
                     Response::new()
                         .status(404)
-                        .text(format!("File `{}` not found", file_path)),
-                )
+                        .text(format!("Folder `{}` not found", file_path)),
+                ))
             }
-        };
+        }
+        .map(|x| x.unwrap().path())
+        .collect::<Vec<PathBuf>>();
 
-        if req.query.get("download").is_some() {
-            return Some(
-                Response::new()
-                    .bytes(file)
-                    .header(Header::new("Content-Type", "application/octet-stream")),
-            );
+        dir.sort();
+
+        let mut out = String::new();
+
+        if path != PathBuf::from(FILE_SERVE_PATH.clone()) {
+            out.push_str(&format!(
+                r#"<div class="file"><a href="/files{}"><i class="fa fa-folder"></i> ..</a></div>"#,
+                path.parent()?.as_os_str().to_string_lossy().replacen(
+                    FILE_SERVE_PATH.as_str(),
+                    "",
+                    1
+                )
+            ));
         }
 
-        Some(
-            show_response(path)
-                .unwrap_or_else(|| {
-                    Response::new().header(Header::new("Content-Type", "application/octet-stream"))
-                })
-                .bytes(file),
-        )
-    }));
+        for i in dir {
+            let j = i.to_str()?;
+            let url = j.split(&*FILE_SERVE_PATH).nth(1)?.to_owned();
+            let name = j.split(path.to_str()?).nth(1)?.to_owned();
+
+            out.push_str(&format!(
+                r#"<div class="file"><a href="/files{}"><i class="fa fa-{}"></i> {}</a></div>"#,
+                url,
+                match i.is_file() {
+                    true => "file",
+                    _ => "folder",
+                },
+                name.strip_prefix('/')
+                    .unwrap_or(&name)
+                    .strip_prefix('\\')
+                    .unwrap_or(&name)
+            ));
+        }
+
+        return Some(Some(
+            Response::new()
+                .text(
+                    Template::new(
+                        fs::read_to_string("./data/template/files.html")
+                            .unwrap_or_else(|_| "{{FILES}}".to_owned()),
+                    )
+                    .template("PATH", path.file_name()?.to_str()?)
+                    .template("FILES", out)
+                    .template("VERSION", VERSION)
+                    .build(),
+                )
+                .header(Header::new("Content-Type", "text/html; charset=utf-8")),
+        ));
+    }
+
+    let file = match fs::read(&path) {
+        Ok(i) => i,
+        Err(_) => {
+            return Some(Some(
+                Response::new()
+                    .status(404)
+                    .text(format!("File `{}` not found", file_path)),
+            ))
+        }
+    };
+
+    if req.query.get("download").is_some() {
+        return Some(Some(
+            Response::new()
+                .bytes(file)
+                .header(Header::new("Content-Type", "application/octet-stream")),
+        ));
+    }
+
+    Some(Some(
+        show_response(path)
+            .unwrap_or_else(|| {
+                Response::new().header(Header::new("Content-Type", "application/octet-stream"))
+            })
+            .bytes(file),
+    ))
 }
 
 fn show_response(file: PathBuf) -> Option<Response> {
