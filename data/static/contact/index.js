3526fdd39f94f557533041325908f3ded1105414
Connor Slade <connor@connorcode.com>
2021-12-20 19:38:53 -0500

    🐐 Scss Updates

--- data/static/contact/index.js
+++ data/static/contact/index.js
@@ -1,10 +1,16 @@
 // Define Contact Data
 const data = {
-  website : ["https://connorcode.com", "https://connorcode.com"],
-  github  : ["https://github.connorcode.com", "https://github.connorcode.com"],
-  discord : ["Sigma#8214"],
-  email   : ["connor@connorcode.com", "mailto:connor@connorcode.com"],
-  gpg_key : ["https://connorcode.com/key", "https://connorcode.com/key"],
+  website: [
+    [
+      "http://zgurppzm423u2ny7ljdj2m33na4wxoo7zvkvvu4vpce2ure5gplexnqd.onion",
+      "http://zgurppzm423u2ny7ljdj2m33na4wxoo7zvkvvu4vpce2ure5gplexnqd.onion",
+    ],
+    ["https://connorcode.com", "https://connorcode.com"],
+  ],
+  github:  [["https://github.connorcode.com", "https://github.connorcode.com"]],
+  discord: [["Sigma#8214"]],
+  email:   [["connor@connorcode.com", "mailto:connor@connorcode.com"]],
+  gpg_key: [["https://connorcode.com/key", "https://connorcode.com/key"]],
 };
 
 // Remove 'this site requires js' message
@@ -13,7 +19,9 @@ document.getElementById("js").remove();
 // Create Links to click for info
 Object.keys(data).forEach((key) => {
   let element = document.createElement("div");
-  element.innerHTML = `<p>✦ </p><a href="#" class="items" id="${key}">[${key.replace(/_/g, ' ').toUpperCase()}]</a>`;
+  element.innerHTML = `<p>✦ </p><a href="#" class="items" id="${key}">[${key
+    .replace(/_/g, " ")
+    .toUpperCase()}]</a>`;
   element.addEventListener("click", () => {
     showData(key, element);
   });
@@ -25,10 +33,13 @@ function showData(id, node) {
   if (!(id in data)) return;
   let userData = data[id];
   delete data[id];
-  let element = document.createElement("P");
-  element.classList.add("infoValue");
-  element.innerHTML = `➥ ${userData[0]}`;
-  if (userData[1] != undefined)
-    element.innerHTML = `➥ <a href="${userData[1]}" class="userDataLink">${userData[0]}</a>`;
-  node.parentNode.insertBefore(element, node.nextSibling);
+  console.log(userData);
+  userData.forEach((i) => {
+    let element = document.createElement("P");
+    element.classList.add("infoValue");
+    element.innerHTML = `➥ ${i[0]}<br>`;
+    if (i[1] != undefined)
+      element.innerHTML = `➥ <a href="${i[1]}" class="userDataLink">${i[0]}</a><br>`;
+    node.parentNode.insertBefore(element, node.nextSibling);
+  });
 }
