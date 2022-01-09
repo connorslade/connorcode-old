3f8bc3e1b149013652bef057048de79773263b2f
Connor Slade <connor@connorcode.com>
2022-01-09 14:05:00 -0500

    🙊 Make run a method

--- src/files.rs
+++ src/files.rs
@@ -40,7 +40,7 @@ impl Middleware for Files {
             return MiddleRequest::Continue;
         }
 
-        MiddleRequest::Send(run(req).unwrap_or_else(|| {
+        MiddleRequest::Send(self.run(req).unwrap_or_else(|| {
             Response::new()
                 .status(500)
                 .text(
@@ -58,119 +58,119 @@ impl Files {
     pub fn new() -> Self {
         Files
     }
-}
 
-fn run(req: Request) -> Option<Response> {
-    let file_path = req.path.replace("/..", "");
-    let mut file_path = file_path.strip_prefix("/files")?.to_owned();
+    fn run(&self, req: Request) -> Option<Response> {
+        let file_path = req.path.replace("/..", "");
+        let mut file_path = file_path.strip_prefix("/files")?.to_owned();
 
-    while file_path.starts_with('/') {
-        file_path = file_path.replacen("/", "", 1);
-    }
+        while file_path.starts_with('/') {
+            file_path = file_path.replacen("/", "", 1);
+        }
 
-    let path = PathBuf::from(FILE_SERVE_PATH.clone()).join(&file_path);
+        let path = PathBuf::from(FILE_SERVE_PATH.clone()).join(&file_path);
 
-    if path.is_dir() {
-        let mut dir = match fs::read_dir(&path) {
+        if path.is_dir() {
+            let mut dir = match fs::read_dir(&path) {
+                Ok(i) => i,
+                Err(_) => {
+                    return Some(
+                        Response::new()
+                            .status(404)
+                            .text(format!("Folder `{}` not found", file_path)),
+                    )
+                }
+            }
+            .map(|x| x.unwrap().path())
+            .collect::<Vec<PathBuf>>();
+
+            dir.sort();
+
+            let mut out = String::new();
+
+            if path != PathBuf::from(FILE_SERVE_PATH.clone()) {
+                out.push_str(&format!(
+                    r#"<div class="file"><i class="fa fa-folder"></i><a href="/files{}">..</a><p class="size"></p></div>"#,
+                    path.parent()?.to_string_lossy().replacen(
+                        FILE_SERVE_PATH.as_str(),
+                        "",
+                        1
+                    )
+                ));
+            }
+
+            for i in dir {
+                let j = i.to_string_lossy();
+                let url = j.split(&*FILE_SERVE_PATH).nth(1)?.to_owned();
+                let name = j.split(path.to_str()?).nth(1)?.to_owned();
+                let mut size = i.metadata().ok()?.len();
+
+                if i.is_dir() {
+                    if let Some(sub_dir) = fs::read_dir(&i).ok() {
+                        for i in sub_dir.map(|x| x.unwrap().path()).collect::<Vec<PathBuf>>() {
+                            size += i.metadata().ok()?.len();
+                        }
+                    }
+                }
+
+                out.push_str(&format!(
+                    r#"<div class="file"><i class="fa fa-{}"></i><a href="/files{}">{}</a><p class="size">{}</p><p class="modified">{}</p></div>"#,
+                    match i.is_file() {
+                        true => "file",
+                        _ => "folder",
+                    },
+                    url,
+                    name.strip_prefix('/')
+                        .unwrap_or(&name)
+                        .strip_prefix('\\')
+                        .unwrap_or(&name),
+                    best_size(size),
+                    best_time(i.metadata().ok()?.modified().ok()?.elapsed().ok()?.as_secs())
+                ));
+            }
+
+            return Some(
+                Response::new()
+                    .text(
+                        Template::new(
+                            fs::read_to_string("./data/template/files.html")
+                                .unwrap_or_else(|_| "{{FILES}}".to_owned()),
+                        )
+                        .template("PATH", path.file_name()?.to_str()?)
+                        .template("FILES", out)
+                        .template("VERSION", VERSION)
+                        .build(),
+                    )
+                    .header(Header::new("Content-Type", "text/html; charset=utf-8")),
+            );
+        }
+
+        let file = match fs::read(&path) {
             Ok(i) => i,
             Err(_) => {
                 return Some(
                     Response::new()
                         .status(404)
-                        .text(format!("Folder `{}` not found", file_path)),
+                        .text(format!("File `{}` not found", file_path)),
                 )
             }
-        }
-        .map(|x| x.unwrap().path())
-        .collect::<Vec<PathBuf>>();
+        };
 
-        dir.sort();
-
-        let mut out = String::new();
-
-        if path != PathBuf::from(FILE_SERVE_PATH.clone()) {
-            out.push_str(&format!(
-                r#"<div class="file"><i class="fa fa-folder"></i><a href="/files{}">..</a><p class="size"></p></div>"#,
-                path.parent()?.to_string_lossy().replacen(
-                    FILE_SERVE_PATH.as_str(),
-                    "",
-                    1
-                )
-            ));
-        }
-
-        for i in dir {
-            let j = i.to_string_lossy();
-            let url = j.split(&*FILE_SERVE_PATH).nth(1)?.to_owned();
-            let name = j.split(path.to_str()?).nth(1)?.to_owned();
-            let mut size = i.metadata().ok()?.len();
-
-            if i.is_dir() {
-                if let Some(sub_dir) = fs::read_dir(&i).ok() {
-                    for i in sub_dir.map(|x| x.unwrap().path()).collect::<Vec<PathBuf>>() {
-                        size += i.metadata().ok()?.len();
-                    }
-                }
-            }
-
-            out.push_str(&format!(
-                r#"<div class="file"><i class="fa fa-{}"></i><a href="/files{}">{}</a><p class="size">{}</p><p class="modified">{}</p></div>"#,
-                match i.is_file() {
-                    true => "file",
-                    _ => "folder",
-                },
-                url,
-                name.strip_prefix('/')
-                    .unwrap_or(&name)
-                    .strip_prefix('\\')
-                    .unwrap_or(&name),
-                best_size(size),
-                best_time(i.metadata().ok()?.modified().ok()?.elapsed().ok()?.as_secs())
-            ));
-        }
-
-        return Some(
-            Response::new()
-                .text(
-                    Template::new(
-                        fs::read_to_string("./data/template/files.html")
-                            .unwrap_or_else(|_| "{{FILES}}".to_owned()),
-                    )
-                    .template("PATH", path.file_name()?.to_str()?)
-                    .template("FILES", out)
-                    .template("VERSION", VERSION)
-                    .build(),
-                )
-                .header(Header::new("Content-Type", "text/html; charset=utf-8")),
-        );
-    }
-
-    let file = match fs::read(&path) {
-        Ok(i) => i,
-        Err(_) => {
+        if req.query.get("download").is_some() || req.query.get("raw").is_some() {
             return Some(
                 Response::new()
-                    .status(404)
-                    .text(format!("File `{}` not found", file_path)),
-            )
+                    .bytes(file)
+                    .header(Header::new("Content-Type", "application/octet-stream")),
+            );
         }
-    };
 
-    if req.query.get("download").is_some() {
-        return Some(
-            Response::new()
-                .bytes(file)
-                .header(Header::new("Content-Type", "application/octet-stream")),
-        );
+        Some(
+            show_response(path)
+                .unwrap_or_else(|| {
+                    Response::new().header(Header::new("Content-Type", "application/octet-stream"))
+                })
+                .bytes(file),
+        )
     }
-
-    Some(
-        show_response(path)
-            .unwrap_or_else(|| {
-                Response::new().header(Header::new("Content-Type", "application/octet-stream"))
-            })
-            .bytes(file),
-    )
 }
 
 /// Convert a Byte size into the biggest unit
