28ddf8dea032466f664f6802d8604051b52a9053
Connor Slade <connor@connorcode.com>
2022-01-10 17:14:29 -0500

    🧮 Finish About

--- data/static/about/index.js
+++ data/static/about/index.js
@@ -1,9 +1,10 @@
 document.querySelector("#greeting").innerHTML = getGreeting();
+document.querySelector("#email").innerHTML =
+  '<a href="mailto:connor@connorcode.com">connor@connorcode.com</a>';
 
 function getGreeting() {
   let hour = new Date().getHours();
+  if (hour >= 17) return "Good evening";
+  if (hour >= 12) return "Good afternoon";
   if (hour >= 3) return "Good morning";
-  if (hour >= 7) return "Good afternoon";
-  return "Good evening";
 }
-  
