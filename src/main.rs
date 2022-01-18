f255d785c75c49dcaf31cf2e33c33b62b8ded50f
Connor Slade <connor@connorcode.com>
2022-01-18 18:54:33 -0500

    📅 Remove 'Database'

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,4 @@
 use std::env;
-use std::sync::Mutex;
 use std::time::Duration;
 
 use afire::{Header, Logger, Middleware, Response, Server};
@@ -17,21 +16,15 @@ mod assets;
 mod common;
 mod components;
 mod config;
-mod database;
 mod files;
 mod middleware;
 use analytics::Analytics;
 use color::Color;
 use config::{SERVER_HOST, SERVER_PORT};
-use database::Database;
 use files::Files;
 use template::Template;
 
 pub const VERSION: &str = "6.0.2";
-
-lazy_static! {
-    pub static ref DB: Mutex<Database> = Mutex::new(init_db());
-}
 
 fn main() {
     let args: Vec<String> = env::args().collect();
@@ -47,22 +40,6 @@ fn main() {
             Color::Green
         )
     );
-
-    // Init Database Stuff
-    lazy_static::initialize(&DB);
-
-    ctrlc::set_handler(move || {
-        color_print!(Color::Cyan, "\n[*] Saveing Database");
-        DB.lock().unwrap().save().unwrap();
-        std::process::exit(0);
-    })
-    .expect("Error setting Ctrl-C handler");
-
-    std::thread::spawn(|| loop {
-        std::thread::sleep(Duration::from_secs(60 * 60));
-        println!("[*] Saveing Database");
-        DB.lock().unwrap().save().unwrap();
-    });
 
     // Setup HTTP Server
     let mut server = Server::new(&*SERVER_HOST, *SERVER_PORT)
@@ -115,34 +92,4 @@ fn print_info() {
     color_print!(Color::Magenta, " └── Other");
     color_print!(Color::Magenta, "     ├── Status Serve: {}", config::STATUS_SERVE);
     color_print!(Color::Magenta, "     └── Onion Brodcast: {}", config::BROADCAST_ONION);
-}
-
-fn init_db() -> Database {
-    let path = std::path::Path::new("./data/data.db");
-    let mut db = if path.exists() {
-        color_print!(Color::Cyan, "[*] Loadine Database");
-        database::Database::load(path).unwrap()
-    } else {
-        color_print!(Color::Cyan, "[*] Creating Database");
-        database::Database::new(path)
-    };
-
-    for i in ["main", "writing_likes"] {
-        if !db.table_exists(i).unwrap() {
-            db.table(i).unwrap();
-        }
-    }
-
-    if !db.value_exists("main", "reads").unwrap() {
-        db.set("main", "reads", 0);
-    }
-
-    db.get_set(
-        "main",
-        "reads",
-        Box::new(|c| Some(c.parse::<u32>().ok()? + 1)),
-    )
-    .unwrap();
-
-    db
 }
