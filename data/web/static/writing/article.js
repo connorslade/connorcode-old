b06ab3eb88c679978e36b601fcabf409ed84c1c7
Connor Slade <connor@connorcode.com>
2022-02-23 19:39:35 -0500

    ✍ Writing Table of Contents

--- data/web/static/writing/article.js
+++ data/web/static/writing/article.js
@@ -32,7 +32,7 @@ function loadTime(time) {
 
 function setLikes(likes, ogLiked) {
   isLiked = ogLiked;
-  let likeButton = document.querySelector(".like");
+  let likeButton = document.querySelector("[like-button]");
   if (likes == null) return;
 
   updateLikesUI(likes, ogLiked);
@@ -44,7 +44,7 @@ function setLikes(likes, ogLiked) {
 }
 
 function updateLikesUI(likes, ogLiked) {
-  let likeContent = document.querySelector(".like-content");
+  let likeContent = document.querySelector("[like-content]");
 
   if (isLiked && !ogLiked) likes = likes + 1;
   if (!isLiked && ogLiked) likes = likes - 1;
@@ -66,5 +66,40 @@ function updateLikesApi(isLiked) {
     headers: {
       "Content-Type": "application/json",
     },
+  });
+}
+
+const TAGS = ["H1", "H2", "H3", "H4", "H5"];
+function initContents() {
+  let contentsButton = [
+    document.querySelector("[contents-button]"),
+    document.querySelector("[contents-button-icon]"),
+  ];
+  let contentsDropdown = document.querySelector("[contents-dropdown]");
+  let elements = Array.from(document.querySelector("article").children);
+
+  elements.forEach((e) =>
+    TAGS.forEach((eName, offset) => {
+      if (e.tagName !== eName) return;
+
+      let ele = document.createElement("div");
+      ele.innerText = e.innerText;
+      ele.style.paddingLeft = `${2 * offset}em`;
+
+      ele.addEventListener("click", () => {
+        window.location = e.children[0].href;
+      });
+
+      contentsDropdown.appendChild(ele);
+    })
+  );
+
+  document.addEventListener("click", (e) => {
+    if (contentsButton.includes(e.target)) {
+      contentsDropdown.classList.toggle("hidden");
+      return;
+    }
+
+    contentsDropdown.classList.add("hidden");
   });
 }
