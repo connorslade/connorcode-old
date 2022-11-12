6d1244dfe2c499ad7b6a3db3c03a16a4f996db26
Connor Slade <connor@connorcode.com>
2022-11-11 22:23:02 -0500

    Finish control

--- src/control.rs
+++ src/control.rs
@@ -6,17 +6,13 @@ use crate::App;
 /*
  - update-articles | Reloads articles from disk
  - update-links    | Reloads links from disk
- - update-home     | Reloads projects for home page
 */
 
 pub fn attach(server: &mut Server<App>) {
-    // idk
     let app = server.state.as_ref().unwrap().clone();
     let link_app = app.clone();
-    let home_app = app.clone();
 
     RemoteControl::new()
-        .enabled(true)
         .any(|req, data| {
             println!(
                 "[R] Control `{}` from `{}`",
@@ -26,7 +22,6 @@ pub fn attach(server: &mut Server<App>) {
         })
         .system("update-articles", move |_| ok(|| app.reload_articles()))
         .system("update-links", move |_| ok(|| link_app.reload_links()))
-        // .system("update-home", move |_| ok(home_app.clone()))
         .attach(server);
 }
 
