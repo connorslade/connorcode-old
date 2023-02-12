1d40699df09427f0b0586fd5e13d5ce408d2f8df
Connor Slade <connor@connorcode.com>
2023-02-12 10:48:50 -0500

    Fix build

--- src/serve_static.rs
+++ src/serve_static.rs
@@ -29,16 +29,6 @@ lazy_static! {
 pub fn attach(server: &mut Server<App>) {
     let data_dir = &server.state.as_ref().unwrap().config.data_dir;
     ServeStatic::new(data_dir.to_string_lossy())
-        // Inject version into responses
-        // Replaces `{{VERSION}}` with the verison in main.rs
-        // TODO: Find a workaround for this - It was kinda a bad idea to start with
-        // .middleware(|_req, res, suc| match String::from_utf8(res.data.clone()) {
-        //     Ok(i) => Some((
-        //         res.bytes(i.replace("{{VERSION}}", VERSION).into_bytes()),
-        //         suc,
-        //     )),
-        //     Err(_) => None,
-        // })
         // Set content encoding to utf-8
         .middleware(|_req, res, _suc| {
             if let Some(i) = res.headers.get_mut(HeaderType::ContentType) {
@@ -70,7 +60,7 @@ pub fn not_found(path: &str) -> Response {
     Response::new()
         .status(404)
         .text(
-            fs::read_to_string("data/web/dist-template/not_found.html")
+            fs::read_to_string("web/dist/template/not_found.html")
                 .unwrap_or_else(|_| "Not Found :/".to_owned())
                 .replace("{{VERSION}}", VERSION)
                 .replace("{{PAGE}}", path),
