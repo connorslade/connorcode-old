c361528c26fe2b8da7f258a9d532b5ceba4a587f
Connor Slade <connor@connorcode.com>
2023-04-29 19:12:43 -0400

    Update deps

--- src/control.rs
+++ src/control.rs
@@ -1,5 +1,6 @@
 use afire::Server;
 use afire_integration::RemoteControl;
+use serde_json::Value;
 
 use crate::App;
 
@@ -13,15 +14,14 @@ pub fn attach(server: &mut Server<App>) {
     let link_app = app.clone();
 
     RemoteControl::new()
-        .any(|req, data| println!("[R] Control `{}` from `{}`", data.action, req.address))
         .system("update-articles", move |_| ok(|| app.reload_articles()))
         .system("update-links", move |_| ok(|| link_app.reload_links()));
     // .attach(server);
 }
 
-fn ok(exe: impl Fn()) -> String {
+fn ok(exe: impl Fn()) -> Value {
     exe();
-    "Ok".to_owned()
+    Value::String("Ok".to_owned())
 }
 
 // TODO: Article caching
